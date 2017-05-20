510b80a57c1972124ad7b6b50aa835ffb3d36764
Joseph Anthony <anthonyj7458@gmail.com>
2017-05-20 12:04:46 +0530

    Hulk Mailer initialized

--- config/default.js
+++ config/default.js
@@ -1,3 +1,29 @@
 module.exports = {
-  title: 'Hulk Mailer Demo Service'
+  title: 'Hulk Mailer Demo Service',
+  email_providers: [
+    {
+      name: "ironman",
+      provider: "mailgun",
+      apiKey: "key-imagined",
+      domain: "sandboxed.domain.mailgun.org"
+    },
+    {
+      name: "thor",
+      provider: "mandrill",
+      apiKey: "sandboxedkey"
+    },
+    {
+      name: "hawkeye",
+      provider: "sendgrid",
+      username: "sandboxed",
+      password: "sandboxedkey"
+    },
+    {
+      name: "captainamerica",
+      provider: "ses",
+      accessKeyId: "sandboxedkey",
+      secretAccessKey: "sandboxedAccesskey",
+      region: "us-east-1"
+    }
+  ]
 }
