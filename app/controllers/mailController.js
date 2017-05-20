48910318307e4b2fc45c7ffa8013b5e4700e1e22
Joseph Anthony <anthonyj7458@gmail.com>
2017-05-20 17:20:42 +0530

    Added Mail Controller for mail/send

--- app/controllers/mailController.js
+++ app/controllers/mailController.js
@@ -1,4 +1,13 @@
+import HulkMailer from 'hulk-mailer';
+import Responder from '../../lib/expressResponder';
+
 export default class MailController {
   static send(req, res) {
+    const email = new HulkMailer.Email(req.body);
+    HulkMailer.send(email, (error, response) => {
+      if(error)
+        return Responder.operationFailed(res, error);
+      return Responder.success(res, response);
+    });
   }
 };
