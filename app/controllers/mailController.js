334d3cb009f1d61b2c4bd6705d7f59bb9f20c426
Joseph Anthony <anthonyj7458@gmail.com>
2017-05-23 15:24:25 +0530

    Separated developmental dependencies

--- app/controllers/mailController.js
+++ app/controllers/mailController.js
@@ -4,10 +4,10 @@ import Responder from '../../lib/expressResponder';
 export default class MailController {
   static send(req, res) {
     const email = new HulkMailer.Email(req.body);
-    HulkMailer.send(email, (error, response) => {
+    HulkMailer.send(email, (error, success) => {
       if(error)
         return Responder.operationFailed(res, error);
-      return Responder.success(res, response);
+      return Responder.success(res, success);
     });
   }
 };
