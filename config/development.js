378c45623976844246eec492e0d8f69e2127c990
Joseph Anthony <anthonyj7458@gmail.com>
2017-05-24 21:01:20 +0530

    Git ignored production configs

--- config/development.js
+++ config/development.js
@@ -1,3 +1,29 @@
 module.exports = {
-  port: 3000
+  port: 3000,
+  email_providers: [
+    {
+      name: "ironman",
+      provider: "mailgun",
+      apiKey: "key-imagined",
+      domain: "sandboxed.domain.mailgun.org"
+    },
+    {
+      name: "thor",
+      provider: "mandrill",
+      apiKey: "sandboxedkey"
+    },
+    {
+      name: "hawkeye",
+      provider: "sendgrid",
+      username: "sandboxed",
+      password: "sandboxedkey"
+    },
+    {
+      name: "captainamerica",
+      provider: "ses",
+      accessKeyId: "sandboxedkey",
+      secretAccessKey: "sandboxedAccesskey",
+      region: "us-east-1"
+    }
+  ]
 }
